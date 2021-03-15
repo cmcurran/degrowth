a8e6a6a7dbfaa5d5a7d4c65c415fffda08b1df6e
Charlotte Curran <charlotte@telness.se>
2021-03-15 18:14:34 +0100

    initial commit, adds styled-components and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <Wrapper>THE DEGROWTH</Wrapper>;
+};
+
+const Wrapper = styled.div`
+  text-transform: uppercase;
+  font-family: "Suisse";
+  background: pink;
+  font-size: 142px;
+`;
 
 export default App;
